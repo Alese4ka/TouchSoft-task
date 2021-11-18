2dcdab713653fee91a2b5609ad8547cf182b96ca
Alese4ka <alesun4ik@gmail.com>
2021-11-18 14:07:15 +0300

    init: -create second task

---
+++ second.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  let newName;
+  if(name === 'Mark') {
+    newName = `Hi, ${name}`; 
+    return newName;
+  } else {
+    newName = `Hello, ${name}`;
+    return newName;
+  }
+}
